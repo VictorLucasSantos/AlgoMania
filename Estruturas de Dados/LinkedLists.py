873053147f1aee533fd063be424ca49fb9f39387
Victor Lucas Santos Gonzaga <victorlucas.santos124@gmail.com>
2024-06-05 22:37:12 -0400

    Módulo 2
    - Adiciona arquivos para estudos de tipos de estruturas de dados

---
+++ Estruturas de Dados/LinkedLists.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.value = value
+class LinkedList:
+    def __init__(self):
+        pass
+    def insert_node_to_tail(self, node):
+        pass
+    def insert_node_to_head(self, node):
+        pass
+    def isempty(self): 
+        return self.head is None
+    def head(self):
+        LinkedList[0]
+    def tail(self):
+        LinkedList[-1]
