c5da8a35335a04c089a3e3af41a84d86c3b6050b
Victor Lucas <victorlucas.santos124@gmail.com>
2025-03-23 19:24:43 -0400

    Heuristicas E Tecnicas

---
+++ Heuristicas E Tecnicas/Deep First Seach.py
@@ -0,0 +1,36 @@
+maze = []
+
+
+class Node:
+    value = 0
+    left = None
+    right = None
+
+
+def simple_deep_first_seach(node, value_to_find):
+    if not node:
+        return False
+    if node.value == value_to_find:
+        return True
+    return deep_first_seach(node.left, value_to_find) or deep_first_seach(
+        node.left, value_to_find
+    )
+
+
+def find_next_position(position, visited_positions):
+    return 0, 0
+
+
+def item_value(position):
+    return maze[position[0], position[1]]
+
+
+def deep_first_seach(position, visited_positions, value_to_find):
+    if item_value(position) == value_to_find:
+        return True
+
+    next_position = find_next_position(position, visited_positions)
+    if next_position:
+        return deep_first_seach(
+            next_position, visited_positions + [position], value_to_find
+        )
